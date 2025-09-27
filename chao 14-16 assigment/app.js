f64adcf927c9e7e7acc453e21237f7f77428e09a
muhammadahmed144 <muhammadahmedmohsin123@gmail.com>
2025-09-27 14:06:27 +0500

    update assigment

---
+++ chao 14-16 assigment/app.js
@@ -0,0 +1,18 @@
+// CHAPTER NO 14-16
+// QUESTION NO 1
+let student = []
+// QUESTION NO 2
+let students = new Array()
+// QUESTION NO 3
+let stringarray = ["html","css","javascript"]
+// QUESTION NO 4
+let numberarray = [10,20,30]
+// QUESTION NO 5
+let booleanarray = [true,false,null]
+// QUESTION NO 6
+let mixedarray = ["javascript",100,true,]
+// QUESTION NO 7
+let educationqualification = ["SSC","HSC","BCS","BS","BCOM","MS","Phil","PhD"]
+for(i = 0; i <educationqualification.length; i++){
+    document.writeln((i+1) + ") " +educationqualification[i]+"</br>")
+}
